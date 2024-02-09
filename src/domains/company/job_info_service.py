41a602abc237658a904794064b6c4517d3928194
TerrenceChao <rtyufjvbn@gmail.com>
2024-02-09 16:41:02 +0800

    add get_all_continents_and_countries API

--- src/domains/company/job_info_service.py
+++ src/domains/company/job_info_service.py
@@ -28,8 +28,18 @@ class JobInfoService:
     def get_continents(self) -> (ContinentListVO):
         continent_vos = self.__get_continent_vos()
         return ContinentListVO(continents=continent_vos)
+    
+    def __get_country_vos(self, continent_name: str) -> (List[CountryVO]):
+        countries = my_world.get_countries_data_of(continent_name)
+        return list(map(
+            lambda x: CountryVO(
+                code=x.get('ISO3', 'unknown'),
+                name=x.get('Country Name', 'unknown'),
+            ),
+            countries,
+        ))
 
-    def get_countries(self, continent_code: str) -> (CountryListVO):
+    def get_countries(self, continent_code: str) -> (List[CountryListVO]):
         continent_iter = filter(
             lambda x: x.continent_code == continent_code,
             self.__get_continent_vos()
@@ -40,15 +50,19 @@ class JobInfoService:
             raise ClientException(
                 f'Continent code {continent_code} is not found.')
 
-        countries = my_world.get_countries_data_of(continent_vo.continent_name)
-        countries = list(
-            map(lambda x: CountryVO(
-                code=x.get('ISO3', 'unknown'),
-                name=x.get('Country Name', 'unknown'),
+        country_vos = self.__get_country_vos(continent_vo.continent_name)
+        return [CountryListVO(
+            countries=country_vos,
+            continent=continent_vo,
+        )]
+
+    def get_all_continents_and_countries(self) -> (List[CountryListVO]):
+        continent_vos = self.__get_continent_vos()
+        continent_country_vos = list(map(
+            lambda continent: CountryListVO(
+                continent=continent,
+                countries=self.__get_country_vos(continent.continent_name),
             ),
-                countries)
-        )
-        return CountryListVO(
-            countries=countries,
-            continent=continent_vo
-        )
+            continent_vos
+        ))
+        return continent_country_vos
--- src/routers/v1/job_info.py
+++ src/routers/v1/job_info.py
@@ -24,6 +24,14 @@ def get_continents():
     return res_success(data=data)
 
 
+# TODO: this route rule(get_all_continents_and_countries) is same as 'get_countries',
+# so it has to be put before 'get_countries'
+@router.get('/continents/all/countries')
+def get_all_continents_and_countries():
+    data = _job_info_service.get_all_continents_and_countries()
+    return res_success(data=data)
+
+
 @router.get('/continents/{continent_code}/countries')
 def get_countries(continent_code: str):
     data = _job_info_service.get_countries(continent_code)
